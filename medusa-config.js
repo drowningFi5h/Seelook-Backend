2837ffd010ae9c0ffb11849b02fa71b3be50a0ba
Tausif <tausifmdansari1@gmail.com>
2024-10-23 22:18:22 +0530

    redis installed and fixed the home button

--- medusa-config.js
+++ medusa-config.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 
+
 let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
   case "production":
@@ -55,18 +56,13 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
-    resolve: "@medusajs/event-bus-redis",
-    options: {
-      redisUrl: REDIS_URL
-    }
-  },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: process.env.CACHE_REDIS_URL,
+
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -77,7 +73,7 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: process.env.CACHE_REDIS_URL,
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
